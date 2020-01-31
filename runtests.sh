bba9e438250873524f3659809c6b06cb92f9254e
samvaran kashyap rallabandi <samvarankashyap@users.noreply.github.com>
2020-01-31 11:05:08 -0500

    add os specific dependency installation

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 echo "Hello this is first attempt to run bash script on github actions"
+if grep -iq centos "/etc/os-release"; then
+  echo "This is a centos machine"
+  yum install -y epel-release;
+  yum install -y python3-pip;
+fi
+if grep -iq fedora "$File"; then
+  echo "This is a fedora machine"
+  yum install -y python3-pip;
+fi
