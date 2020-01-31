6b43f0c166542323e175a123171414721b08c2fb
samvaran kashyap rallabandi <samvarankashyap@users.noreply.github.com>
2020-01-31 11:34:46 -0500

    update var name

--- runtests.sh
+++ runtests.sh
@@ -2,7 +2,7 @@
 echo "Hello this is first attempt to run bash script on github actions"
 OS=$(cat /etc/*release | grep ^NAME | tr -d 'NAME="') 
 echo $OS
-if [[ $act = "CentOS Linux" ]]
+if [[ $OS = "CentOS Linux" ]]
 then
     echo "This is centos";
     yum install -y epel-release;
