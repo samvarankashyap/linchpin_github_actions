3dcb75b6d43554f595a1886f53f3002807b2f367
samvaran kashyap rallabandi <samvarankashyap@users.noreply.github.com>
2020-01-31 11:45:17 -0500

    added which to deps

--- runtests.sh
+++ runtests.sh
@@ -5,9 +5,9 @@ echo $OS
 if [[ $OS = "CentOS Linux" ]]
 then
     echo "This is centos";
-    yum install -y epel-release;
-    yum install -y python3-pip python3-flake8;
+    yum install -y epel-release which;
+    yum install -y python3-pip python3-flake8 python3-devel gcc;
 else
     echo "This is fedora"
-    yum install -y python3-pip python3-flake8 python3-devel gcc;
+    yum install -y python3-pip python3-flake8 python3-devel gcc which;
 fi
