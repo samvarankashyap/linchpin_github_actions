f2da52fe60cbe9dadb8ab7e7e28ea9c1810031b1
samvaran kashyap rallabandi <samvarankashyap@users.noreply.github.com>
2020-01-31 11:31:25 -0500

    install python3 flake8

--- runtests.sh
+++ runtests.sh
@@ -6,8 +6,8 @@ if [[ $act = "Centos" ]]
 then
     echo "This is centos";
     yum install -y epel-release;
-    yum install -y python3-pip;
+    yum install -y python3-pip python3-flake8;
 else
     echo "This is fedora"
-    yum install -y python3-pip;  
+    yum install -y python3-pip python3-flake8;
 fi
