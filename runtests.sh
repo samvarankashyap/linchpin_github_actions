b7ac5b17001bcc795f29e81132941c35832cd6ff
samvaran kashyap rallabandi <samvarankashyap@users.noreply.github.com>
2020-01-31 11:25:26 -0500

    os based installations

--- runtests.sh
+++ runtests.sh
@@ -2,12 +2,12 @@
 echo "Hello this is first attempt to run bash script on github actions"
 OS=$(cat /etc/*release | grep ^NAME | tr -d 'NAME="') 
 echo $OS
-if /bin/grep -iq centos "/etc/os-release"; then
-  echo "This is a centos machine"
-  yum install -y epel-release;
-  yum install -y python3-pip;
-fi
-if /bin/grep -iq fedora "$File"; then
-  echo "This is a fedora machine"
-  yum install -y python3-pip;
+if [[ $act = "Centos" ]]
+then
+    echo "This is centos";
+    yum install -y epel-release;
+    yum install -y python3-pip;
+else
+    echo "This is fedora"
+    yum install -y python3-pip;  
 fi
