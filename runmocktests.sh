9aab57032f14c70275d72cf234305229dcce7eb5
samvaran kashyap rallabandi <samvarankashyap@users.noreply.github.com>
2020-02-18 11:41:55 -0500

    add run mock tests.sh

---
+++ runmocktests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo "Hello this is first attempt to run linchpin provisioning in github actions"
+
+linchpin --version;
+
+mkdir /tmp/workspace/;
+
+cd /tmp/workspace/;
+
+echo $PWD;
